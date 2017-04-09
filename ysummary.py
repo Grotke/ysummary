4cb659e768bb7f3416484f4c566a41ed3b25a0ea
Joseph Montgomery <joseph.conan.montgomery@gmail.com>
2017-04-09 02:08:22 -0400

    Add video class and abstract video layout

--- ysummary.py
+++ ysummary.py
@@ -10,18 +10,29 @@ env = Environment(
 )
 
 class Channel:
-    def __init__(self, name, videoUrl):
+    def __init__(self, name, channelUrl, videos):
         self.name = name
+        self.channelUrl = channelUrl
+        self.videos = videos
+
+class Video:
+    def __init__(self, title, videoUrl, thumbnail, thumbAltText):
+        self.title = title
         self.videoUrl = videoUrl
+        self.thumbnail = thumbnail
+        self.thumbAltText = thumbAltText
 
 
 @app.route('/home.html')
 def home():
+    videos1 = [Video("Cats", "", "https://i.ytimg.com/vi/rHgCTRB3hfU/maxresdefault.jpg", "Alt Text")]
+    videos2 = [Video("Animu Girls", "", "https://i.ytimg.com/vi/rHgCTRB3hfU/maxresdefault.jpg", "Jesse Cox Alt Text")]
+    videos3 = [Video("Baking", "", "", "")]
     return render_template('home.html', 
         channels=[
-        Channel("Seananners", "https://www.youtube.com/user/SeaNanners/videos"), 
-        Channel("Jesse Cox", "https://www.youtube.com/user/OMFGcata/videos"),
-        Channel("Cupquake", "http://www.colgate.com/en/us/oc/")])
+        Channel("Seananners", "https://www.youtube.com/user/SeaNanners/videos", videos1), 
+        Channel("Jesse Cox", "https://www.youtube.com/user/OMFGcata/videos", videos2),
+        Channel("Cupquake", "http://www.colgate.com/en/us/oc/", videos3)])
 
 
 
